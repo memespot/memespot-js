6e319fefa24c01fe409f7712da6ddfd879a3fbb7
cameo-js <kjs8469@gmail.com>
2015-11-22 00:21:33 +0900

    기능 개선
    - 이미지 레이지 로딩 구현 #6
    - 클립보드 카피 기능 추가

---
+++ extension/assets/js/copy-clipboard.js
@@ -0,0 +1,29 @@
+function copyTextToClipboard(text) {
+  var result = false;
+  var textArea = document.createElement("textarea");
+  
+  textArea.style.position = 'fixed';
+  textArea.style.top = 0;
+  textArea.style.left = 0;
+  textArea.style.width = '2em';
+  textArea.style.height = '2em';
+  textArea.style.padding = 0;
+  textArea.style.border = 'none';
+  textArea.style.outline = 'none';
+  textArea.style.boxShadow = 'none';
+  textArea.style.background = 'transparent';
+
+  textArea.value = text;
+  document.body.appendChild(textArea);
+  textArea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    result = true;
+  } catch (err) {
+  }
+
+  document.body.removeChild(textArea);
+  return result;
+}
--- src/suggest/memespot-sug.js
+++ src/suggest/memespot-sug.js
@@ -17,6 +17,8 @@ export default class Suggest {
     this.focusBoard = false;
     this.focusIndex = 0;
     this.count = 0;
+    this.nextUrl = null;
+    this.isLazyLoding = false;
   }
   initialize() {
     this.$target.keyup(e => {
@@ -24,14 +26,36 @@ export default class Suggest {
       this.keyControll(e.keyCode);
       if( value != this.value ) {
         this.value = value;
-        $.getJSON(API_URL + value, this.loadSuggestData.bind(this));
+        $("#board").html();
+        this.apiCall(API_URL + value, this.loadSuggestData);
       }
     });
+    this.$board.scroll(this.lazyLoad.bind(this));
+  }
+  apiCall(url, callback){
+    $.getJSON(url, callback.bind(this));
   }
   loadSuggestData(data){
-    this.count = data.results.length;
+    if(!!data.next){
+      this.nextUrl = data.next.replace(/jQuery[0-9_]*/, "?");
+      this.isLazyLoding = true;
+    } else {
+      this.nextUrl = null;
+    }
+
     var images = data.results.map( result => { return result.url; });
-    ReactDOM.render(<ImageList images={images} onImageClick={this.onImageClick.bind(this)} />, document.getElementById('board'));
+    var div = document.createElement("div");
+    ReactDOM.render(<ImageList images={images} onImageClick={this.onImageClick.bind(this)} />, div);
+    
+    this.$board.append($(div));
+    this.count = this.$board.find("img").length;
+  }
+  lazyLoad(){
+    if(this.isLazyLoding && !!this.nextUrl && (this.$board.height() - this.$board.scrollTop() < 200 )){
+      this.isLazyLoding = false;
+      console.log("call");
+      this.apiCall(this.nextUrl, this.loadSuggestData);
+    }
   }
   keyControll(keyCode){
     switch (keyCode){
@@ -49,8 +73,8 @@ export default class Suggest {
         if( this.focusBoard ) {
           if( this.focusIndex < Display.WIDTH_SIZE ) {
             this.focusBoard = false;
-            this.focusIndex = 0;
             this.$board.find("img").eq(this.focusIndex).css("border","");
+            this.focusIndex = 0;
           } else {
             this.move(-Display.WIDTH_SIZE);
             this.$board.animate({ scrollTop: `${this.$board.scrollTop() - Display.IMAGE_HEIGHT}` });
@@ -64,6 +88,7 @@ export default class Suggest {
           } else {
             this.move(Display.WIDTH_SIZE);
             this.$board.animate({ scrollTop: `${this.$board.scrollTop() + Display.IMAGE_HEIGHT}` });
+            this.lazyLoad();
           }
         } else {
           this.focusBoard = true;
@@ -72,8 +97,9 @@ export default class Suggest {
         }
         break;
       case Commands.ENTER :
-        if( this.focusBoard ) {console.log(this.$board.find("img").eq(this.focusIndex).data("url"));
+        if( this.focusBoard ) {
           this.$result.val(this.$board.find("img").eq(this.focusIndex).data("url"));
+          this.copyToClipboard(this.$board.find("img").eq(this.focusIndex).data("url"));
         }
         break;
     }
@@ -89,5 +115,13 @@ export default class Suggest {
     this.focusIndex = num;
     this.$board.find("img").eq(this.focusIndex).css("border","2px solid blue");
     this.$result.val(this.$board.find("img").eq(this.focusIndex).data("url"));
+    this.copyToClipboard(this.$board.find("img").eq(this.focusIndex).data("url"));
+  }
+  copyToClipboard(text){
+    if(copyTextToClipboard(text)){
+      $("#copyMessage").show();
+      $("#copyMessage").css("opacity",0.1);
+      $("#copyMessage").animate({opacity:1.0},1300,function(){$("#copyMessage").hide()})
+    }
   }
 }
